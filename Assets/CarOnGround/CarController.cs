8863888b5db7d049030c3efffc8ba0ba139f090d
Keval Patel <kevalpatel2106@gmail.com>
2018-04-05 19:21:59 +0530

    Adding car on image target with door open/close

---
+++ Assets/CarOnGround/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+		
+	private bool isSoundPlayed = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		if(!isSoundPlayed && transform.localPosition.y < .05f){
+			isSoundPlayed = true;
+			StartCoroutine (DelaySound ());
+		}
+	}
+
+	public void MoveCar(){
+		transform.localPosition += new Vector3 (0, 10, 0);
+		transform.eulerAngles += new Vector3 (5, 20, 5);
+		isSoundPlayed = false;
+	}
+
+	IEnumerator DelaySound(){
+		yield return new WaitForSeconds (.2f);
+		GetComponent<AudioSource> ().Play ();
+	}
+}
---
+++ Assets/CarOnGround/DoorMovementController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovementController : MonoBehaviour {
+
+	public bool isLeftDoor = false;
+
+	public AudioSource openAudioSource;
+	public AudioSource closeAudioSource;
+
+	private float currentAngle = 0f;
+	private float desiredAngle = 0f;
+
+	// Update is called once per frame
+	void Update () {
+		currentAngle = Mathf.LerpAngle (currentAngle, desiredAngle, Time.deltaTime * 3f);
+		transform.localEulerAngles = new Vector3 (0, currentAngle, 0);
+	}
+
+	void OpenDoors(){
+		if (isLeftDoor) {
+			desiredAngle = 60f;
+		} else {
+			desiredAngle = -60f;
+		}
+
+		openAudioSource.Play ();
+	}
+
+	void CloseDoors(){
+		desiredAngle = 0f;
+		StartCoroutine (DelayCarCloseSound ());
+	}
+
+	IEnumerator DelayCarCloseSound(){
+		yield return new WaitForSeconds (0.3f);
+		closeAudioSource.Play ();
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if(collider.CompareTag("MainCamera")){
+			OpenDoors ();
+		}
+	}
+		
+	void OnTriggerExit(Collider collider){
+		if(collider.CompareTag("MainCamera")){
+			CloseDoors ();
+		}
+	}
+}
